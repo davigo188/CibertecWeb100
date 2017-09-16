13870a440bfd6ad4840a1134c4a8858114b34264
davigo188wp <davigo188wp@hotmail.com>
2017-09-16 14:04:03 -0500

    Clase 20170916

---
+++ Cibertec/Cibertec.RepositoriesTests/CustomerRepositoryTest.cs
@@ -0,0 +1,25 @@
+using Cibertec.Models;
+using Cibertec.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Xunit;
+
+namespace Cibertec.RepositoriesTests
+{
+    public class CustomerRepositoryTest
+    {
+        private readonly DbContext _context;
+        public CustomerRepositoryTest()
+        {
+            _context = new NorthwindDBContext();
+        }
+
+        [Fact(DisplayName ="[CustomerRepository]Get All")]
+        public void Customer_Repository_GetAll()
+        {
+            var repo = new RepositoryEF<Customer>(_context);
+            var result = repo.GetList();
+            Assert.True(result.Count() > 0);
+        }
+    }
+}
