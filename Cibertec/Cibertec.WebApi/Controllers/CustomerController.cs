e7dfbf0d783d0e435fdf84b7e0dbe09ec59b6301
davigo188wp <davigo188wp@hotmail.com>
2017-09-30 14:00:34 -0500

    Implementacion de Web Api

---
+++ Cibertec/Cibertec.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cibertec.UnitOfWork;
+
+namespace Cibertec.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Customer")]
+    public class CustomerController : Controller
+    {
+        private readonly IUnitOfWork _unit;
+
+        public CustomerController(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public IActionResult GetList()
+        {
+            return Ok(_unit.Customers.GetList());
+        }
+
+
+    }
+}
