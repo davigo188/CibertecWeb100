8cd942ab709729bf4a783ec79dcc9e1dc21c27a1
davigo188wp <davigo188wp@hotmail.com>
2017-10-21 13:52:47 -0500

    Javascript - Practica 1 y 2

---
+++ Cibertec/Cibertec.Html/scripts/index.js
@@ -0,0 +1,14 @@
+﻿(function (cibertec) {
+    cibertec.Index = {
+        currentYear: function () {
+            var today = new Date();
+            return today.getFullYear();
+        },
+
+        currentMonth: function () {
+            var today = new Date();
+            return length(today.getMonth();
+        }
+    };
+    document.getElementById("date").innerHTML = cibertec.Index.currentYear() + ' ' + cibertec.Index.currentMonth();
+})(window.cibertec = window.cibertec || {});
