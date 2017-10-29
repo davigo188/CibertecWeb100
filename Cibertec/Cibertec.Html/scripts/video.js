41d8512f58df3d100da65bd9dce682021754cf82
davigo188wp <davigo188wp@hotmail.com>
2017-10-28 21:50:28 -0500

    Se completa la Guia_de_Laboratorio_1_2

--- Cibertec/Cibertec.Html/scripts/video.js
+++ Cibertec/Cibertec.Html/scripts/video.js
@@ -4,19 +4,15 @@
         play: function () {
             if (this.videoElement.paused) {
                 this.videoElement.play();
-
             }
         },
         pause: function () {
             if (this.videoElement.played) {
                 this.videoElement.pause();
-
             }
         },
         stop: function () {
             this.videoElement.currentTime = 0;
-
-
         }
     };
 })(window.cibertec = window.cibertec || {});
