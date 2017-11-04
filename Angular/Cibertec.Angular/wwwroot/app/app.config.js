6f00610f2a5f31711bdf271116dda5c6188e58fa
davigo188wp <davigo188wp@hotmail.com>
2017-11-04 14:29:28 -0500

    Se implementa Angular

---
+++ Angular/Cibertec.Angular/wwwroot/app/app.config.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+    angular.module('app').run(run);
+
+    run.$inject = ['$http', '$state', 'localStorageService', 'configService'];
+
+    function run($http, $state, localStorageService, configService) {
+        var user = localStorageService.get('userToken');
+        if (user && user.token) {
+            $http.defaults.headers.common.Autorization = 'Bearer' + localStorageService.get('userToken').token;
+            configService.setLogin(true);
+        }
+        else $state.go('login');
+    }
+
+})();
+
