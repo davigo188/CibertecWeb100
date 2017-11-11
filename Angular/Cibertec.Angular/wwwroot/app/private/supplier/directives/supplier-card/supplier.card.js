e1c582134feac1697d4ed4fab35133c669c9d264
davigo188wp <davigo188wp@hotmail.com>
2017-11-11 14:02:58 -0500

    .

---
+++ Angular/Cibertec.Angular/wwwroot/app/private/supplier/directives/supplier-card/supplier.card.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+    angular.module('app')
+        .directive('customerCard', customerCard);
+    function customerCard() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                id: '@',
+                companyName: '@',
+                contactName: '@',
+                contactTitle: '@',
+                country: '@',
+                phone: '@'
+            },
+            templateUrl: 'app/private/supplier/directives/supplier-card/supplier-card.html'
+
+        };
+    }
+})();
